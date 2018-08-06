171fa82628bb539113d7ce1c027aba58fc4d9348
zhu <534092674@qq.com>
2018-08-06 14:08:39 +0800

    maoyanzz

--- maoyan/src/components/cinema.vue
+++ maoyan/src/components/cinema.vue
@@ -1,19 +1,31 @@
 <template>
 	<div>
-		cinema
+		<ul>
+			<li v-for="data in datalist">
+				<h2>{{data.nm}}</h2>
+				<h4>{{data.addr}}</h4>
+			</li>
+		</ul>
 	</div>
 </template>
 
 
 <script type="text/javascript">
+    import axios from "axios";
 	import router from "../router"
 	export default{
 		name:"cinema",
 		data(){
 			return{
-
+				datalist:[]
 			}
 		},
+		mounted(){
+			axios.get("/ajax/cinemaList?day=2018-08-06&offset=0&limit=20&districtId=-1&lineId=-1&hallType=-1&brandId=-1&serviceId=-1&areaId=-1&stationId=-1&item=&updateShowDay=true&reqId=1533512399172&cityId=65").then(res=>{
+				console.log(res.data)
+				this.datalist =res.data.cinemas
+			})
+		}
 	}
 
 
@@ -27,5 +39,8 @@
 			width:100%;
 			height:100%;
 		}
+	li{
+		border-bottom: 1px solid #ccc;
+	}
 
 </style>
